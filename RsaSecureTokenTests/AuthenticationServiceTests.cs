3242b1b1530b30bf6c80f15f377172793cc80bb5
hatelove <jay.hatelove@gmail.com>
2018-04-18 17:01:26 +0800

    Add project files.

---
+++ RsaSecureTokenTests/AuthenticationServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RsaSecureToken;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RsaSecureToken.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+
+            var actual = target.IsValid("joey", "91000000");
+
+            //always failed
+            Assert.IsTrue(actual);                       
+        }
+    }
+}
